b65eeab648e95d143aed7e1900e9185a4f2e1ec0
stanislav <stanislavt@kicb.net>
2020-01-07 16:52:01 +0600

    add auto fill drivers. Also try to use converter

--- src/main/java/stanislav/tun/novinomad/picasso/controllers/TourController.java
+++ src/main/java/stanislav/tun/novinomad/picasso/controllers/TourController.java
@@ -3,6 +3,7 @@ package stanislav.tun.novinomad.picasso.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,6 +19,7 @@ import stanislav.tun.novinomad.picasso.persistance.services.TourService;
 import javax.validation.Valid;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 
 @Controller
 @RequestMapping("/tours")
@@ -26,23 +28,30 @@ public class TourController {
     TourService tourService;
 
     @Autowired
-    DriverService driverService;
+    private DriverService driverService;
 
     @Autowired
     ObjectMapper mapper;
+
+    Driver d1 = new Driver("Stas", "Wong");
+    Driver d2 = new Driver("Roman", "Frik");
+    Driver d3 = new Driver("Tolik", "Rakh");
 
     @RequestMapping("/add")
     public ModelAndView getAddTourView(Model model) {
         var map = new HashMap<String, Object>();
 
         map.put("tourForm", new Tour());
+        driverService.createDriver(d1);
+        driverService.createDriver(d2);
+        driverService.createDriver(d3);
         map.put("drivers", driverService.getDriversList());
 
         return new ModelAndView("addTourPage.html", map);
     }
 
     @RequestMapping("/list")
-    public ModelAndView getToursListView(Model model){
+    public ModelAndView getToursListView(Model model) {
         var mav = new ModelAndView("toursListPage.html");
         mav.addObject("toursList", tourService.getAllTours());
         return mav;
@@ -50,7 +59,7 @@ public class TourController {
 
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String addTourAction(@ModelAttribute("tourForm") @Valid Tour tour, Model model) {
+    public String addTourAction(@ModelAttribute("tourForm") @Valid Tour tour, @ModelAttribute("drivers") List<Driver> drivers, Model model) {
 
         try {
             System.out.printf(mapper.writeValueAsString(tour));
@@ -60,8 +69,14 @@ public class TourController {
         var start = tour.getStartDate();
         var end = tour.getEndDate();
 
-        if(start != null && end != null)
+        if (start != null && end != null)
             tour.setDays(end.get(Calendar.DAY_OF_YEAR) - start.get(Calendar.DAY_OF_YEAR));
+
+        if (!drivers.isEmpty())
+            for (Driver d : drivers) {
+                System.out.println("UAHAHAHAH " + d.toString());
+                tour.addDriver(d);
+            }
 
         tourService.createTour(tour);
         model.addAttribute("driver", tour);
