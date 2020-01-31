56fb57069bfb70ae99d24ea696b8d68516a5fe05
stas <wong.stanislav@gmail.com>
2020-01-31 23:25:30 +0600

    refactor(GuideController): added addGuideView method

--- src/main/java/stanislav/tun/novinomad/picasso/controllers/GuideController.java
+++ src/main/java/stanislav/tun/novinomad/picasso/controllers/GuideController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import stanislav.tun.novinomad.picasso.persistance.pojos.Guide;
 import stanislav.tun.novinomad.picasso.persistance.services.GuideService;
 
 @Controller
@@ -22,6 +23,16 @@ public class GuideController {
         var modelAndView = new ModelAndView();
         modelAndView.addObject("guides", guides);
         modelAndView.setViewName("guidesListPage.html");
+        return modelAndView;
+    }
+
+    // get view
+    @RequestMapping("/add")
+    public ModelAndView addGuideView() {
+        var guide = new Guide();
+        var modelAndView = new ModelAndView();
+        modelAndView.addObject("guide", guide);
+        modelAndView.setViewName("addGuidePage.html");
         return modelAndView;
     }
 }
