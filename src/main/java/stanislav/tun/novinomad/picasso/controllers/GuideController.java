2ebeaa3e37fc6874a0bfd1f1f0e554566d6faee3
stas <wong.stanislav@gmail.com>
2020-01-31 23:29:31 +0600

    refactor(GuideController): added getEditGuideView and addOrUpdateGuideAction methods

--- src/main/java/stanislav/tun/novinomad/picasso/controllers/GuideController.java
+++ src/main/java/stanislav/tun/novinomad/picasso/controllers/GuideController.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import stanislav.tun.novinomad.picasso.persistance.pojos.Guide;
 import stanislav.tun.novinomad.picasso.persistance.services.GuideService;
+import stanislav.tun.novinomad.picasso.util.JsonPrinter;
 
 @Controller
 @RequestMapping("/guides")
@@ -34,5 +37,23 @@ public class GuideController {
         modelAndView.addObject("guide", guide);
         modelAndView.setViewName("addGuidePage.html");
         return modelAndView;
+    }
+
+    @RequestMapping(value="/edit{id}")
+    public ModelAndView getEditGuideView(@PathVariable(value = "id") Long guideId){
+        var guide = guideService.getGuide(guideId);
+        var mav = new ModelAndView();
+        mav.addObject("guide", guide);
+        mav.setViewName("addGuidePage.html");
+        return mav;
+    }
+
+    // action todo: make single method for adding guide to db
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String addOrUpdateGuideAction(Guide guide) {
+        logger.debug("guide to be created or updated = "+ JsonPrinter.getString(guide));
+        guideService.createOrUpdateGuide(guide);
+
+        return "redirect:/guides/add";
     }
 }
