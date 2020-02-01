c36d3cf11584f950fff59b2d932e8d1e035e8a68
stas <wong.stanislav@gmail.com>
2020-02-01 21:56:54 +0600

    feat(GuideIntervalService): added service for guide tour interval

---
+++ src/main/java/stanislav/tun/novinomad/picasso/persistance/services/GuideIntervalService.java
@@ -0,0 +1,57 @@
+package stanislav.tun.novinomad.picasso.persistance.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import stanislav.tun.novinomad.picasso.persistance.pojos.*;
+import stanislav.tun.novinomad.picasso.persistance.repositories.IGuideIntervalRepo;
+
+import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class GuideIntervalService {
+    Logger logger = LoggerFactory.getLogger(GuideIntervalService.class);
+
+    @Autowired
+    IGuideIntervalRepo repo;
+
+    @Transactional
+    public void createOrUpdateInterval( GuideTourIntervals interval) {
+        repo.save(interval);
+    }
+
+    public Optional<GuideTourIntervals> getInterval(long id) {
+        return repo.findById(id);
+    }
+
+    public Collection<GuideTourIntervals> getAllRelatedToDriver(Guide guide) {
+        //return Collections.sort(Arrays.asList(repo.findByDriverEquals(driverId)));
+        return repo.findByGuideEquals(guide);
+    }
+
+    public Collection<GuideTourIntervals> getAllRelatedToTour(Tour tour) {
+        //return Collections.sort(Arrays.asList(repo.findByDriverEquals(driverId)));
+        return repo.findByTourEquals(tour);
+    }
+
+    public Collection<GuideTourIntervals> getAllRelatedToTourAndGuide(Tour tour, Guide guide) {
+        //return Collections.sort(Arrays.asList(repo.findByDriverEquals(driverId)));
+        return repo.findByGuideAndTourEquals(guide, tour);
+    }
+
+    public Collection<GuideTourIntervals> getAllIntervals() {
+        return (Collection<GuideTourIntervals>) repo.findAll();
+    }
+
+    public void delete(GuideTourIntervals guideTourIntervals){
+        repo.delete(guideTourIntervals);
+    }
+
+    public void delete(Long id){
+        repo.deleteById(id);
+    }
+}
