4e4f73c11bab703f038b7c841ef7a9fa70d59604
stas <wong.stanislav@gmail.com>
2020-02-26 21:15:27 +0600

    rf: build.gradle added webjars jquery + bootstrap rf: mainLayout.html fixed 404 error while upload scripts from webjars

--- src/main/java/stanislav/tun/novinomad/picasso/config/WebConfig.java
+++ src/main/java/stanislav/tun/novinomad/picasso/config/WebConfig.java
@@ -6,8 +6,6 @@
 //public class WebConfig implements WebMvcConfigurer {
 //    @Override
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry
-//                .addResourceHandler("/webjars/**")
-//                .addResourceLocations("/webjars/");
+//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 //    }
 //}
