8c709bb258de718b66e27e7c7ffd6b357735237c
stanislav <stanislavt@kicb.net>
2020-03-13 17:35:14 +0600

    rf: MySessionListener fixed no value present exception

--- src/main/java/stanislav/tun/novinomad/picasso/security/MySessionListener.java
+++ src/main/java/stanislav/tun/novinomad/picasso/security/MySessionListener.java
@@ -1,16 +1,21 @@
 package stanislav.tun.novinomad.picasso.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.web.session.HttpSessionDestroyedEvent;
 import org.springframework.stereotype.Component;
+import stanislav.tun.novinomad.picasso.PicassoApp;
 import stanislav.tun.novinomad.picasso.persistance.pojos.User;
 import stanislav.tun.novinomad.picasso.persistance.services.UserService;
 import stanislav.tun.novinomad.picasso.util.ConcurrentHolder;
 
 @Component
 public class MySessionListener implements ApplicationListener<HttpSessionDestroyedEvent> {
+    Logger log = LoggerFactory.getLogger(MySessionListener.class);
+
     @Autowired
     ConcurrentHolder holder;
 
@@ -27,7 +32,11 @@ public class MySessionListener implements ApplicationListener<HttpSessionDestroy
             var user = userService.getUser(currentAuditor.get().toString());
             if(!user.isEmpty() && user.isPresent()){
                 holder.release(user.get());
+            }else{
+                log.warn("Unable to get current user! Release entities was not executed.");
             }
+        }else{
+            log.warn("Unable to get current auditor! Release entities was not executed.");
         }
     }
 }
